b296ba27704ad17d08862bb5b3190d19c2246e19
Craig Wechtler <cwechtler@yahoo.com>
2019-02-24 13:48:16 -0600

    Add Bootstrap & Button Function

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var playButton = document.querySelector(".btn");
 
-var diceImage1 = document.querySelector(".img1");
-var diceImage2 = document.querySelector(".img2");
+playButton.onclick = function(){ Roll();};
 
-diceImage1.setAttribute("src", "images/dice" + randomNumber1 + ".png");
-diceImage2.setAttribute("src", "images/dice" + randomNumber2 + ".png");
+function Roll(){
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-if(randomNumber1 > randomNumber2){
-  document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
-}
-else if(randomNumber1 < randomNumber2){
-  document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
-}
-else if(randomNumber1 === randomNumber2){
-  document.querySelector("h1").textContent = "🚩 Draw! 🚩";
+  var diceImage1 = document.querySelector(".img1");
+  var diceImage2 = document.querySelector(".img2");
+
+  diceImage1.setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  diceImage2.setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+  if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+  }
+  else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+  }
+  else if(randomNumber1 === randomNumber2){
+    document.querySelector("h1").textContent = "🚩 Draw! 🚩";
+  }
 }
