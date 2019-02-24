ea8f180081cb1bf0dd09e9956c395eeec852fcc4
Craig Wechtler <cwechtler@yahoo.com>
2019-02-24 13:40:52 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var diceImage1 = document.querySelector(".img1");
+var diceImage2 = document.querySelector(".img2");
+
+diceImage1.setAttribute("src", "images/dice" + randomNumber1 + ".png");
+diceImage2.setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if(randomNumber1 > randomNumber2){
+  document.querySelector("h1").textContent = "🚩 Player 1 Wins!";
+}
+else if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").textContent = "Player 2 Wins! 🚩";
+}
+else if(randomNumber1 === randomNumber2){
+  document.querySelector("h1").textContent = "🚩 Draw! 🚩";
+}
